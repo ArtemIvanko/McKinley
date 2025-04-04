4d0ac51ff0e1e7456d66efaf8577daad98229232
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-04-04 10:54:10 +0300

    Fixed styles for image grid. Added container

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -4,33 +4,48 @@ import styled from "@/DefaultTheme";
 
 export const Wrapper = () => (
   <Root>
-    <ImageContainer>
-      <Image src={CityImage} alt="City" loading="lazy" />
-    </ImageContainer>
-    <TextContainer>
-      <Typography variant="h3">Corporate Legal Representation</Typography>
-      <Typography variant="body2">
-        We understand the difficulties and stress that your legal issues bring.
-        Our team is committed to providing you with the individual attention,
-        communication, and dedication you deserve.
-      </Typography>
-      <Button variant="contained">Request Consultation</Button>
-    </TextContainer>
+    <StyledContainer>
+      <ImageContainer>
+        <Image src={CityImage} alt="City" loading="lazy" />
+      </ImageContainer>
+      <TextContainer>
+        <Typography variant="h3">Corporate Legal Representation</Typography>
+        <Typography variant="body2">
+          We understand the difficulties and stress that your legal issues
+          bring. Our team is committed to providing you with the individual
+          attention, communication, and dedication you deserve.
+        </Typography>
+        <Button variant="contained">Request Consultation</Button>
+      </TextContainer>
+    </StyledContainer>
   </Root>
 );
 
 const Root = styled("div")(({ theme }) => ({
   display: "flex",
-  flexDirection: "column",
+  justifyContent: "center",
   alignItems: "center",
-  gap: "2rem",
-  textAlign: "center",
   backgroundColor: theme.palette.secondary.light,
   padding: "2rem 1rem",
   [theme.breakpoints.up("lg")]: {
-    padding: "5rem 4rem",
-    flexDirection: "row-reverse",
-    justifyContent: "space-between",
+    padding: "5rem 1rem",
+  },
+}));
+
+const StyledContainer = styled("div")(({ theme }) => ({
+  display: "grid",
+  gridTemplateColumns: "1fr",
+  gridTemplateAreas: `
+    "image"
+    "text"
+  `,
+  gap: "2rem",
+  textAlign: "center",
+  maxWidth: "1200px",
+  [theme.breakpoints.up("lg")]: {
+    gridTemplateColumns: "1fr 1fr",
+    gridTemplateAreas: `"text image"`,
+    alignItems: "center",
     textAlign: "left",
   },
 }));
@@ -40,6 +55,7 @@ const TextContainer = styled("div")(({ theme }) => ({
   flexDirection: "column",
   alignItems: "center",
   gap: "2rem",
+  gridArea: "text",
   [theme.breakpoints.up("lg")]: {
     alignItems: "flex-start",
   },
@@ -50,18 +66,14 @@ const ImageContainer = styled("div")(({ theme }) => ({
   justifyContent: "center",
   alignItems: "center",
   width: "100%",
-  height: "auto",
+  gridArea: "image",
+  overflow: "hidden",
   [theme.breakpoints.up("lg")]: {
-    height: "522px",
-    width: "fit-content",
+    aspectRatio: "1 / 1",
   },
 }));
 
-const Image = styled("img")(({ theme }) => ({
+const Image = styled("img")(() => ({
   width: "100%",
-  height: "auto",
-  [theme.breakpoints.up("lg")]: {
-    width: "100%",
-    height: "100%",
-  },
+  height: "100%",
 }));
